31ecc0272822323774b33689f41dfe4d830b336d
juntao_mu <2016402773@qq.com>
2019-08-01 10:18:22 +0800

    适配公共controller

---
+++ third-part-server/src/main/java/com/jinyafu/thirdpart/controller/BaseController.java
@@ -0,0 +1,36 @@
+package com.jinyafu.thirdpart.controller;
+
+import com.jinyafu.thirdpart.config.CompanyDispatch;
+import com.jinyafu.thirdpart.handler.HandlerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 类功能介绍
+ * @CreateDate: 2019/8/1 9:46
+ * @Copyright版权所有 深圳金雅福控股集团有限公司
+ * @Author: mjt
+ * @Version: 1.0
+ */
+@RestController
+public class BaseController {
+    @Autowired
+    CompanyDispatch companyDispatch;
+
+    /**
+     * @return com.jinyafu.jmall.pay.handler.PayHandlerAdapter
+     * @throws
+     * @description: 获取适配资源
+     * @params [psyType]
+     * @author lean.yang
+     * @date 2019/7/11
+     */
+    public HandlerAdapter getHandler(String payType) {
+        for (HandlerAdapter adapter : companyDispatch.getHandlerAdapters()) {
+            if (adapter.supports(payType)) {
+                return adapter;
+            }
+        }
+        return null;
+    }
+}
---
+++ third-part-server/src/main/java/com/jinyafu/thirdpart/controller/MailController.java
@@ -0,0 +1,14 @@
+package com.jinyafu.thirdpart.controller;
+
+/**
+ * @Description: 类功能介绍
+ * @CreateDate: 2019/8/1 9:45
+ * @Copyright版权所有 深圳金雅福控股集团有限公司
+ * @Author: mjt
+ * @Version: 1.0
+ */
+public class MailController extends BaseController {
+
+
+
+}
