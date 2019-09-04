4801f324f5a2b3a21439f43c01eb56159c4dba87
wenbin_wu@jinyafu.com <wenbin_wu@jinyafu.com>
2019-09-04 19:16:46 +0800

    宽带实体

---
+++ third-part-server/src/main/java/com/jinyafu/jmall/entity/third/broadband/Broadband.java
@@ -0,0 +1,17 @@
+package com.jinyafu.jmall.entity.third.broadband;/**
+ * @Description: 类功能介绍
+ * @CreateDate: 2019/9/4 19:11
+ * @Copyright版权所有 深圳金雅福控股集团有限公司
+ * @Author: wwb
+ * @Version: 1.0
+ */
+
+/**
+ * @program: third-part-server-Broadband
+ * @description: 宽带
+ * @author: Mr.Wwb
+ * @create: 2019-09-04 19:11
+ **/
+public class Broadband {
+
+}
